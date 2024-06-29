9df41a2e14e4fd7114ddeedb99cfbce0b482a8ca
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2024-06-29 10:34:59 -0500

    Adds colors and fonts from design guide

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryNutmeg: "hsl(14, 45%, 36%)",
+        primaryDarkRaspberry: "hsl(332, 51%, 32%)",
+        neutralWhite: "hsl(0, 0%, 100%)",
+        neutralRoseWhite: "hsl(330, 100%, 98%)",
+        neutralEggshell: "hsl(30, 54%, 90%)",
+        neutralLightGrey: "hsl(30, 18%, 87%)",
+        neutralWengeBrown: "hsl(30, 10%, 34%)",
+        neutralDarkCharcoal: "hsl(24, 5%, 18%)",
+      },
+      fontFamily: {
+        outfit: ["outfit", "sans-serif"],
+        youngSerif: ["young serif", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
